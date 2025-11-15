240f5c48b82df78f429386977d1ebe81f007fd39
minhdq <minhdq1991@gmail.com>
2025-11-15 11:20:34 +0700

    config mail

---
+++ src/main/java/com/bk/service/MailService.java
@@ -0,0 +1,31 @@
+package com.bk.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    //For user
+    public void sendSimpleEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+
+    //For admin
+    public void sendSimpleEmail(String[] to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
--- src/main/java/com/bk/service/UserServiceImpl.java
+++ src/main/java/com/bk/service/UserServiceImpl.java
@@ -5,7 +5,9 @@ import com.bk.entity.MetricEntity;
 import com.bk.repo.AuthLogRepository;
 import com.bk.repo.MetricRepository;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,9 +17,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Service
-@AllArgsConstructor
 public class UserServiceImpl implements UserService {
-
+    @Value("${system.config.mail.admin}")
+    private String mailAdmin;
+    @Value("${system.config.mail.user.enable}")
+    private String enableSendMailUser;
+    @Autowired
+    private MailService mailService;
     @Autowired
     private AuthLogRepository authLogRepository;
     @Autowired
@@ -29,18 +35,26 @@ public class UserServiceImpl implements UserService {
     public void detectAbnormal() {
         var allAuthLogs = authLogRepository.findAll();
         List<MetricEntity> metricEntities = new ArrayList<>();
+        List<Integer> userIdAbnormal = new ArrayList<>();
         for (AuthLogEntity e : allAuthLogs) {
             long start = System.currentTimeMillis();
             var metric = new MetricEntity();
             metric.setUserId(e.getUserId());
             metric.setAnomaly(e.getAnomaly());
             var predictData = externalApiService.callCheckLoginDetect(e);
+            if (1 == predictData.getPrediction()) {
+                userIdAbnormal.add(e.getUserId());
+            }
             metric.setPredict(predictData.getPrediction());
             long end = System.currentTimeMillis();
             metric.setType(1);
             metric.setTook(end - start);
             metric.setModel(predictData.getModel());
             metricEntities.add(metric);
+        }
+        if(StringUtils.isNotBlank(mailAdmin) && !userIdAbnormal.isEmpty()) {
+            String[] mailList = mailAdmin.split(";");
+            mailService.sendSimpleEmail(mailList, "[System Alert] Abnormal login detect!!!", "Abnormal login detect for user list below: " + userIdAbnormal);
         }
         metricRepository.saveAll(metricEntities);
     }
@@ -50,7 +64,7 @@ public class UserServiceImpl implements UserService {
         var allAuthLogs = authLogRepository.findAll();
 
         ExecutorService executor = Executors.newFixedThreadPool(100); // Run 10 request at the same time
-
+        List<Integer> userIdAbnormal = new ArrayList<>();
         List<CompletableFuture<MetricEntity>> futures = allAuthLogs.stream()
                 .map(e ->
                         CompletableFuture.supplyAsync(() -> {
@@ -61,7 +75,9 @@ public class UserServiceImpl implements UserService {
                             metric.setAnomaly(e.getAnomaly());
 
                             var predictData = externalApiService.callCheckLoginDetect(e);
-
+                            if (1 == predictData.getPrediction()) {
+                                userIdAbnormal.add(e.getUserId());
+                            }
                             long end = System.currentTimeMillis();
                             metric.setType(100);
                             metric.setPredict(predictData.getPrediction());
@@ -76,7 +92,10 @@ public class UserServiceImpl implements UserService {
         List<MetricEntity> result = futures.stream()
                 .map(CompletableFuture::join)
                 .toList();
-
+        if(StringUtils.isNotBlank(mailAdmin) && !userIdAbnormal.isEmpty()) {
+            String[] mailList = mailAdmin.split(";");
+            mailService.sendSimpleEmail(mailList, "[System Alert] Abnormal login detect!!!", "Abnormal login detect for user list below: " + userIdAbnormal);
+        }
         metricRepository.saveAll(result);
 
         executor.shutdown();
