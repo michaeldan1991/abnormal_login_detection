fd97bf465b7c98f4ecc57510502a81856592dace
minhdq <minhdq1991@gmail.com>
2025-12-20 21:38:13 +0700

    upload project

---
+++ src/main/resources/script/user.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user (
+                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      username VARCHAR(50) NOT NULL UNIQUE,
+                      password VARCHAR(255) NOT NULL,
+                      firstname VARCHAR(50) NOT NULL,
+                      lastname VARCHAR(50) NOT NULL,
+                      email VARCHAR(100) NOT NULL UNIQUE,
+                      address TEXT,
+                      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                          ON UPDATE CURRENT_TIMESTAMP
+);
